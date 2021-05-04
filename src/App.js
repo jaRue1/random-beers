318db9d375454f602748b7cfd1da4833e0dd46e7
jaRue1 <jaruejohnson@outlook.com>
2021-05-04 10:44:22 -0400

    created a woriking react app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react"
 
 function App() {
+  const [beerList, setBeerList] = useState(null)
+  const [randomBeer, setRandomBeer] = useState(null)
+  // let randomBeer = null
+  useEffect(() => {
+    fetch(`https://beers-api-bc.web.app/beers`)
+    .then((response) => response.json())
+    .then((data) => setBeerList(data))
+    .catch((error) => console.log(error))
+  }, [])
+  const chooseRando = () =>{
+    const rando = Math.floor(Math.random()* beerList.length)
+    setRandomBeer(beerList[rando].name)
+    console.log ({randomBeer})
+
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Beers</h1>
+        {beerList && <button onClick={()=> chooseRando()}> Pick A Beer</button>}
+        {randomBeer && <h2>{randomBeer}<hr/></h2>}
+        {!beerList ? <p>Loading......</p> : beerList.map(beer => {
+      return <p key={beer.id}>{beer.name}</p>
+    })}
       </header>
     </div>
   );
